1bb0093d23e1d0b04d39ea590e9b1fd37d3bf9ee
VWA Software internet <henk@vwa.nu>
2019-06-04 08:03:51 +0200

    Storelocator changes

--- src/Merchello.FastTrack.Ui/App_Plugins/Merchello/client/js/merchello.ui.js
+++ src/Merchello.FastTrack.Ui/App_Plugins/Merchello/client/js/merchello.ui.js
@@ -338,7 +338,33 @@ MUI.AddItem = {
                         data: $(this).serialize()
                     }).then(function(result) {
                         MUI.emit('AddItem.added', result);
-                        MUI.Notify.info(MUI.AddItem.addItemSuccess);
+						MUI.Notify.info(MUI.AddItem.addItemSuccess);
+
+						if (dataLayer) {
+
+							// Measure adding a product to a shopping cart by using an 'add' actionFieldObject
+							// and a list of productFieldObjects.
+							dataLayer.push({
+								'event': 'addToCart',
+								'ecommerce': {
+									'currencyCode': 'EUR',
+									'add': {                                // 'add' actionFieldObject measures.
+										'products': [{                        //  adding a product to a shopping cart.
+											'name': 'Triblend Android T-Shirt',
+											'id': '12345',
+											'price': '15.25',
+											'brand': 'Google',
+											'category': 'Apparel',
+											'variant': 'Gray',
+											'quantity': 1
+										}]
+									}
+								}
+							});
+
+						}
+						
+
                     }, function(err) {
                        MUI.Logger.captureError(err); 
                     });
--- src/Merchello.FastTrack/Controllers/Membership/CustomerMembershipController.cs
+++ src/Merchello.FastTrack/Controllers/Membership/CustomerMembershipController.cs
@@ -38,7 +38,7 @@
 	/// for store implementations will vary.
 	/// </remarks>
 	[PluginController("FastTrack")]
-	public class CustomerMembershipController : CustomerMembershipControllerBase
+	public class CustomerMembershipController : SurfaceController
 	{
 		/// <summary>
 		/// The <see cref="IMemberService"/>.
